695a1dd9832f7d361358299966747f54cff1ae60
KaloyanDimitrov <kaloyan.dimitrov00@gmail.com>
2017-04-16 14:21:16 +0200

    Frame created.

--- GitHub/src/Main.java
+++ GitHub/src/Main.java
@@ -1,9 +1,19 @@
+import java.awt.Window;
+
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		//Window win = new Window();
+		System.out.println("Hello World!");
+		
+		
 	}
 
 }
